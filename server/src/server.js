157377bf5777a0ac071ea3ebb32b76332399b936
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-06-02 19:21:13 +0300

    connected app to mongo cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://kyryloshyrokovdev:w1XkYJe6NQhwNZYN@nasa-cluster.drxq4oa.mongodb.net/nasa-api-db?retryWrites=true&w=majority&appName=NASA-Cluster";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
